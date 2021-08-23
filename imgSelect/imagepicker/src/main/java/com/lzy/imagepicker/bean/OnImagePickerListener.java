ae6807529413013b3643ef05d17c347b27b841cc
zp110579 <zp110579@163.com>
2021-08-23 14:11:35 +0800

    无

--- imgSelect/imagepicker/src/main/java/com/lzy/imagepicker/bean/OnImagePickerListener.java
+++
@@ -1,9 +0,0 @@
-package com.lzy.imagepicker.bean;
-
-import java.util.ArrayList;
-
-public interface OnImagePickerListener {
-
-    void onImagePickerResult(ArrayList<ImageItem> imageItemArrayList);
-
-}
